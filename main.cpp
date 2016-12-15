77320f47323b45599ed6005460416d861ce414fa
adriantiron <tda_mda@yahoo.com>
2016-12-15 13:12:56 +0200

    Add main.cpp
    Base structure.

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+bool gameOver;
+
+void Setup()
+{
+    gameOver = false;
+}
+
+void Draw()
+{
+
+}
+
+void Input()
+{
+
+}
+
+void Logic()
+{
+
+}
+
+int main()
+{
+    Setup();
+
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+    }
+
+
+    return 0;
+}
