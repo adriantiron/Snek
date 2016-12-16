42e14faac88d7c90b00a675a1a4421bc4d3017f8
adriantiron <tda_mda@yahoo.com>
2016-12-16 18:45:45 +0200

    Snake v1.0
    Base structure and initialization of game.

---
+++ _GIT SNAKE/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+bool gameOver;
+const int height = 20;
+const int width = 40;
+int headX, headY, fruitX, fruitY, score;
+enum eDirection { STOP, UP, DOWN, LEFT, RIGHT };
+eDirection dir;
+
+
+void Setup()
+{
+    gameOver = false;
+    dir = STOP;
+    headX = width / 2;
+    headY = height/ 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+}
+
+void Draw()
+{
+
+}
+
+void Input()
+{
+
+}
+
+void Logic()
+{
+
+}
+
+int main()
+{
+    Setup();
+
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+    }
+
+
+    return 0;
+}
