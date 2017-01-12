d629ed63003b0a707695005bf0102c1121fb3451
adriantiron <tda_mda@yahoo.com>
2017-01-12 20:44:04 +0200

    Update v3.1
    Added an apple sprite.

--- _GIT SNAKE/sprites.h
+++ _GIT SNAKE/sprites.h
@@ -70,44 +70,49 @@ void bodySprite(int x , int y)
 void fruitSprite(int x , int y , int z)
 {
      int i , j , a[10][10]={
-      0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
-      0 , 0 , 0 , 2 , 2 , 2 , 2 , 0 , 0 , 0 ,
-      0 , 0 , 2 , 2 , 3 , 3 , 2 , 2 , 0 , 0 ,
-      0 , 2 , 2 , 3 , 2 , 2 , 3 , 2 , 0 , 0 ,
-      0 , 2 , 3 , 2 , 1 , 1 , 2 , 3 , 2 , 0 ,
-      0 , 2 , 3 , 2 , 1 , 1 , 2 , 3 , 2 , 0 ,
-      0 , 2 , 2 , 3 , 2 , 2 , 3 , 2 , 0 , 0 ,
-      0 , 0 , 2 , 2 , 3 , 3 , 2 , 2 , 0 , 0 ,
-      0 , 0 , 0 , 2 , 2 , 2 , 2 , 0 , 0 , 0 ,
-      0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+      0 , 0 , 3 , 3 , 0 , 2 , 2 , 2 , 0 , 0 ,
+      0 , 0 , 0 , 3 , 2 , 2 , 0 , 0 , 0 , 0 ,
+      0 , 1 , 1 , 1 , 2 , 1 , 1 , 1 , 1 , 0 ,
+      1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 ,
+      1 , 1 , 1 , 4 , 1 , 1 , 1 , 1 , 1 , 1 ,
+      1 , 1 , 4 , 1 , 1 , 1 , 1 , 1 , 1 , 1 ,
+      1 , 1 , 4 , 1 , 1 , 1 , 1 , 1 , 1 , 1 ,
+      1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 ,
+      0 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 0 ,
+      0 , 0 , 1 , 1 , 1 , 1 , 1 , 1 , 0 , 0 ,
       };
-      for(i=0;i<10;i++)
-        for(j=0;j<10;j++)
-        if(a[i][j]==1)
-      {
-          setfillstyle(SOLID_FILL , GREEN);
-          bar(x+i,y+j,x+i+1,y+j+1);
-      }
-      else if(a[i][j]==2)
-      {
-          setfillstyle(SOLID_FILL , RED);
-          bar(x+i,y+j,x+i+1,y+j+1);
-      }
-      else if(a[i][j]==3)
-      {
-          setfillstyle(SOLID_FILL , LIGHTRED);
-          bar(x+i,y+j,x+i+1,y+j+1);
-      }
-      else if(a[i][j]==0 && z==1)
-      {
-          setfillstyle(SOLID_FILL , LIGHTGRAY);
-          bar(x+i,y+j,x+i+1,y+j+1);
-      }
-      else if(a[i][j]==0 && z==2)
-      {
-          setfillstyle(SOLID_FILL , BLACK);
-          bar(x+i,y+j,x+i+1,y+j+1);
-      }
+    for(i=0; i<10; i++)
+        for(j=0; j<10; j++)
+            if(a[i][j]==1)
+            {
+                setfillstyle(SOLID_FILL, RED);
+                bar(x+j,y+i,x+j+1,y+i+1);
+            }
+            else if(a[i][j]==2)
+            {
+                setfillstyle(SOLID_FILL, BROWN);
+                bar(x+j,y+i,x+j+1,y+i+1);
+            }
+            else if(a[i][j]==3)
+            {
+                setfillstyle(SOLID_FILL, LIGHTGREEN);
+                bar(x+j,y+i,x+j+1,y+i+1);
+            }
+            else if(a[i][j]==4)
+            {
+                setfillstyle(SOLID_FILL, WHITE);
+                bar(x+j,y+i,x+j+1,y+i+1);
+            }
+            else if(a[i][j]==0 && z==1)
+            {
+                setfillstyle(SOLID_FILL, LIGHTGRAY);
+                bar(x+j,y+i,x+j+1,y+i+1);
+            }
+            else if(a[i][j]==0 && z==2)
+            {
+                setfillstyle(SOLID_FILL, BLACK);
+                bar(x+j,y+i,x+j+1,y+i+1);
+            }
 }
 
 void speedSprite(int x , int y)
@@ -128,7 +133,7 @@ void speedSprite(int x , int y)
         for(j=0; j<10; j++)
             if(a[i][j]==1)
             {
-                setfillstyle(SOLID_FILL, LIGHTGREEN);
+                setfillstyle(SOLID_FILL, LIGHTMAGENTA);
                 bar(x+j,y+i,x+j+1,y+i+1);
             }
             else if(a[i][j]==0)
